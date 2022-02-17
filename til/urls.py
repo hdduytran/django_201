1ca787196eeecf0a8bd84a8437999af6576dd275
hdduytran <19521434@gm.uit.edu.vn>
2022-02-17 18:19:28 +0700

    done

--- til/urls.py
+++ til/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from feed import urls as feed_urls
+from profiles import urls as profiles_urls
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include(feed_urls, namespace= 'feed')),
+    path('profiles/', include(profiles_urls, namespace= 'profiles')),
+    url('', include('allauth.urls')),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
